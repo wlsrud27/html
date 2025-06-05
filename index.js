1b280d88112fa0f311d25074a2402c0a58200eee
DESKTOP-KNGM7GL\902-04 <aaa@aaa.com>
2025-06-05 15:04:04 +0900

    제이쿼리 추가

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-new fullpage('#fullpage', {
-    //options here
+
+    $('#fullpage').fullpage({
     autoScrolling:true,
     scrollHorizontally: true,
     navigation: true,
     navigationPosition: 'right',
-});
+    });
